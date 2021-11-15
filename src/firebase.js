ae4f4fee2cb99eef8065e9f08e550e1bb084d8e8
ASUS <agifkhh5@gmail.com>
2021-11-15 10:17:02 +0700

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,13 @@
 import firebase from 'firebase/app'
 import 'firebase/database'
 
+export const db = firebase
+    .initializeApp({databaseURL : 'https://guidedvuefire0411-57c87-default-rtdb.firebaseio.com/'})
+    .database()
+
+
+export const drinkRef = db.ref('drinks')
+
 
 
 
